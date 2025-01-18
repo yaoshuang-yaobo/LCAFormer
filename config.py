8615f20700f747701d7a804fd1261c4873de8be1
Shuang Yao <74480747+yaoshuang-yaobo@users.noreply.github.com>
2025-01-18 10:57:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,16 +6,16 @@ cfg = __C
 __C.DATA = edict()
 __C.TRAIN = edict()
 __C.LOSS = edict()
-# 程序根目录
+
 __C.DATA.BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 
-# 数据集根目录
+# Datasets root
 __C.DATA.IMAGE_PATH = os.path.join(__C.DATA.BASE_PATH, 'data/Luding/')
-# tensorboard保存路径
+# tensorboard save
 __C.DATA.TENSERBOARD_PATH = os.path.join(__C.DATA.BASE_PATH, 'visualization/tensorboard')
-# 权重存放目录
+# weight files
 __C.DATA.WEIGHTS_PATH = os.path.join(__C.DATA.BASE_PATH, 'weights')
-# 存放预测结果地址
+# test results
 __C.DATA.PRE_PATH = os.path.join(__C.DATA.BASE_PATH, 'Results/')
 
 __C.TRAIN.WEIGHT = 256
@@ -24,16 +24,16 @@ __C.TRAIN.CLASSES = 2
 # batch_size
 __C.TRAIN.BATCH_SIZE = 4
 __C.TRAIN.EPOCHS = 400
-# 设置线程数
+# thread
 __C.TRAIN.WORKERS = 0
-# 是否接着训练
+# Whether to continue training
 __C.TRAIN.RESUME = False
-# 设置学习率
+# learning rate
 # __C.TRAIN.LR = 1e-4
 __C.TRAIN.LR = 0.001800
-# 设置 MOMENTUM
+# MOMENTUM
 __C.TRAIN.MOMENTUM = 0.9
-# 设置weight-decay   default: 5e-4
+# weight-decay   default: 5e-4
 __C.TRAIN.WEIGHT_DECAY = 1e-4
 __C.TRAIN.WARMUP_FACTOR = 1.0 / 3
 __C.TRAIN.WARMUP_ITERS = 0
@@ -46,13 +46,12 @@ __C.TRAIN.LOG_ITER = 10
 __C.TRAIN.LOG_DIR = 'log/'
 
 
-# 设置辅助损失所占的权重
+# The weight of the auxiliary loss
 __C.LOSS.DC= 0.04
 
-# 是否使用ohem的损失函数
+# The weight of the ohem loss
 __C.LOSS.USE_OHEM = False
-# 是否使用辅助损失函数
+# Auxiliary loss
 __C.LOSS.AUX = False
-# 设置辅助损失所占的权重
+# The weight of the auxiliary loss
 __C.LOSS.AUX_WEIGHT = 0.05
-
